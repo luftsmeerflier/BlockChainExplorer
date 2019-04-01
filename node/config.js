d9099a60b6067b867e8922eb7fb20a0d46df0b3d
GTFO <nunya@business.com>
2019-04-01 15:37:55 -0700

    old file structure

--- node/config.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blockchains';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blockchain-app';
-exports.PORT = process.env.PORT || 8080;
